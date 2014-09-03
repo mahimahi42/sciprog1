b94e38f18a1f660adcc2571f18969e4166c2c4dc
Bryce Davis <BAD1128@gmail.com>
2014-09-03 09:39:29 -0400

    Remove old folders

--- old_site/first_day/seniors/Review_bp/Review.java
+++ old_site/first_day/seniors/Review_bp/Review.java
@@ -1,5 +1,31 @@
 import java.util.Scanner;
 
 public class Review {
-    http://bit.ly/sciproggit
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) 
+        {
+            System.out.println(i);
+        }
+        
+        Scanner blahblah = new Scanner(System.in);
+        
+        
+        
+        
+        
+        
+    
+        boolean flag = true;
+        while (flag) {
+            
+            System.out.println("Hello!");
+            flag = false;
+        }
+        
+        if (!flag) {
+            System.out.println("AHH!");
+        } else if (flag) {
+            System.out.println("WOO!");
+        }
+    }
 }
